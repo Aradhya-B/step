6ec03dd1391bbff796d78a069f0f4a2d5f0da553
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-05-27 14:21:40 -0400

    Configure breathing halftone options

--- portfolio/src/main/webapp/scripts/script.js
+++ portfolio/src/main/webapp/scripts/script.js
@@ -24,15 +24,16 @@ const playAudio = (audioClip) => {
 var img = document.querySelector('.portrait img');
 
 new BreathingHalftone( img, {
-  dotSize: 1/200,
+  dotSize: 1/120,
   dotSizeThreshold: 0.01,
   initVelocity: 0.7,
   oscPeriod: 2,
-  channels: [ 'lum' ],
-  friction: 0.25,
-  hoverDiameter: 0.4,
+  oscAmplitude: 0.2,
+  channels: ['green', 'lum'],
+  friction: 0.2,
+  hoverDiameter: 0.3,
   hoverForce: 0.004,
-  activeDiameter: -0.4,
-  activeForce: 0.004
+  activeDiameter: 0.4,
+  activeForce: 0.008
 });
 
