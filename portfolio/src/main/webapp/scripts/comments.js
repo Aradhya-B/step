bef295f636b715ea206f04b336690a9ba2b437ba
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-06-02 22:21:09 +0000

    Update frontend element constructor to create comments instead of list items.
    Comments are formatted with associated style classes.

--- portfolio/src/main/webapp/scripts/comments.js
+++ portfolio/src/main/webapp/scripts/comments.js
@@ -8,14 +8,34 @@ async function fetchAndInsertDataIntoDOM() {
 	commentsListElement.innerHTML = '';
 	comments.forEach(comment => {
 		commentsListElement.appendChild(
-			createListElement(comment)
+			createCommentElement(comment)
 		);
 	});
 }
 
-/** Creates an <li> element containing text */
-function createListElement(text) {
-	const liElement = document.createElement('li');
-	liElement.innerHTML = text;
-	return liElement;
+/** Creates a comment element containing */
+function createCommentElement(comment) {
+	const el = document.createElement('div');
+	el.setAttribute("class", "comment");
+
+	let children = [];
+
+	const dateEl = document.createElement("span");
+	dateEl.innerText = comment.date;
+	dateEl.setAttribute("class", "date");
+	children.push(dateEl);
+
+	const authorEl = document.createElement("a");
+	authorEl.innerText = comment.author;
+	authorEl.setAttribute("href", `mailto:${comment.email}`);
+	authorEl.setAttribute("class", "author");
+	children.push(authorEl);
+
+	const textEl = document.createElement("p");
+	textEl.innerText = comment.comment;
+	textEl.setAttribute("class", "comment-text");
+	children.push(textEl);
+
+	children.forEach(child => el.appendChild(child));
+	return el;
 }
