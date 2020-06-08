d27b81eed2914c899431d89a5595c72792cdc1b1
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-06-08 14:55:38 +0000

    Make footer a reusable element for all pages

---
+++ portfolio/src/main/webapp/scripts/footer.js
@@ -0,0 +1,7 @@
+/*
+ * Fetch footer content from file and 
+ * insert into footer element on page
+ */
+fetch("./footer.html")
+	.then(res => res.text()).
+	then(data => document.querySelector("footer").innerHTML = data)
