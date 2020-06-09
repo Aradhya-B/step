ca68cc685093a4a3d81924a75ca38d10f3264968
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-06-09 20:41:09 +0000

    Add sentiment score as a property of a Comment

--- portfolio/src/main/java/com/google/sps/models/Comment.java
+++ portfolio/src/main/java/com/google/sps/models/Comment.java
@@ -6,6 +6,8 @@ import java.util.Date;
 public class Comment {
   // Comment id
   private long id;
+  // Comment score
+  private double score;
   // Comment author
   private String author;
   // Author email
@@ -15,8 +17,9 @@ public class Comment {
   // Text content of comment
   private String comment;
 
-  public Comment(long id, String author, String email, long time, String comment) {
+  public Comment(long id, double score, String author, String email, long time, String comment) {
     this.id = id;
+    this.score = score;
     this.author = author;
     this.email = email;
     this.time = time;
